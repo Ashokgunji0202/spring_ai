b7ac162aaaed9943824ae887c981f53341c58227
Ashokgunji0202 <ashok.gunji@tekworks.in>
2025-08-30 14:50:40 +0530

    added multimodelproject

---
+++ section_01/multimodels/src/main/java/com/springai/multimodels/MultimodelsApplication.java
@@ -0,0 +1,13 @@
+package com.springai.multimodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimodelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimodelsApplication.class, args);
+	}
+
+}
---
+++ section_01/multimodels/src/test/java/com/springai/multimodels/MultimodelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.multimodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimodelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
